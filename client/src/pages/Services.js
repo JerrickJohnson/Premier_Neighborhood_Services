5e69deb37399a42ef721b51c3f5331fd00927d0d
Alphonse Johnson <a.joh26@gmail.com>
2023-07-30 14:38:02 -0500

    added react-image npm to test out icon/images for services// added code for images to services pages

--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,6 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-import { useQuery } from "@apollo/client";
-import { QUERY_USER } from "../utils/queries";
+// import { useQuery } from "@apollo/client";
+// import { QUERY_USER } from "../utils/queries";
 
+import Image from "react-image";
+
+function App() {
+  return (
+    <Image
+      src="./public/images/soap.jpg"
+      alt="image"
+      loader={<div>Loading...</div>}
+      unloader={<div>Error!</div>}
+    >
+      {(props) => (
+        <div style={{ position: "relative" }}>
+          <img {...props} alt="image2" />
+          <div
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            Text on Image
+          </div>
+        </div>
+      )}
+    </Image>
+  );
+}
+
+export default App;
