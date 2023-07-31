46482a02f46c481445099aa494619fbefab353b0
Alphonse Johnson <a.joh26@gmail.com>
2023-07-31 18:41:15 -0500

    added variables for cleaner code

--- client/src/components/Services/index.js
+++ client/src/components/Services/index.js
@@ -17,41 +17,47 @@ function Dropdown({ options }) {
   );
 }
 function RenderImage() {
+    const styleProp = {
+        backgroundColor: "teal", height: "500px", width: "400px", color:"black"}
+
+
+    const pGraph ="This is filler paragraph to show where everything will line up in the end filler " 
+
   return (
     <div
       style={{ backgroundColor: "grey", height: "1000px" }} //background color is for testing purposes
       className="flex-row"
     >
-      <h1>--Affliated vendors recommeded by Premier Neighborhood Services</h1>
-      <div style={{ backgroundColor: "black", height: "500px", width: "400px", color:"white" }} >
+      <h1>--Affliated vendors recommeded by Premier Neighborhood Services fillerfillerfillerfillerfillerfiller</h1>
+      <div style={ styleProp } >
       <p>
-        This is filler paragrahp to show where everythign will line up in the
-        end
+        {pGraph}
       </p>
 
       </div>
 
-        <div style={{ backgroundColor: "teal", height: "500px", width: "400px", color:"white" }}>
+        <div style={ styleProp} >
         <p>
-            This is filler paragrahp to show where everythign will line up in the
-            end
+            {pGraph}
         </p>
         </div>
 
-        <div style={{ backgroundColor: "teal", height: "500px", width: "400px", color:"white" }}>
+        <div style={styleProp}>
         <p>
-            This is filler paragrahp to show where everythign will line up in the
-            end
+            {pGraph}
         </p>
         </div>
 
       <div className="flex-row"> 
         <div>
-          <img src="images/plumbing-service.jpg" alt="plumber pic"></img>
+            <a href="https:google.com">
+          <img src="images/plumbing-service.jpg" alt="plumber pic"></img> </a>
         </div>
 
         <div>
+            <a href="https:yahoo.com">
           <img style={{height:"65%" ,width: "400px"}} src="images/electric-service.jpg" alt="electrician pic"></img>
+            </a>
         </div>
       </div>
 
--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Dropdown, RenderImage } from "../components/Services";
+// import { useState } from "react";
 
 
 function App() {
@@ -7,16 +8,23 @@ function App() {
     const style = {
         backgroundColor: "grey",
         height: "100px",
+        animationName: "example",
+    animationDuration: "4s",
       };
       const mediaQuery = "(min-width: 768px)";
 
       if (window.matchMedia(mediaQuery).matches) {
         style.height = "200px";
       }
+const options = ["Plumbing", "Electrical", "HVAC", "Carpentry", "Painting"];
+
+    
+
+      
     return (
       <div style={{ style }}>
         <Dropdown
-          options={["Plumbing", "Electrical", "HVAC", "Carpentry", "Painting"] }
+          options={options }
           style ={{backgroundColor: "orange", height: "100px", width: "200px" }}
         />
         <RenderImage />
