bf1271c616e358903fa90704fd783f83207fb11d
JerrickJohnson <jerrickjohnson88@gmail.com>
2023-08-03 18:42:19 -0500

    Able to add product linked by user

--- client/src/pages/AddItem.js
+++ client/src/pages/AddItem.js
@@ -1,13 +1,26 @@
 import React from "react";
-import Cart from "../components/Cart";
+// import Cart from "../components/Cart";
 import AddProductForm from "../components/AddProductForm";
 import { Link } from "react-router-dom";
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
 
 const AddItem = () => {
+  // Fetch the user's data from the server
+  const { loading, data } = useQuery(QUERY_USER);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // Get the user data from the query result
+  const user = data?.user;
+  const loggedInUserId = user?._id || '';
+
   return (
     <div className="container">
         <Link to="/marketplace">← Back to Marketplace</Link>
-        <AddProductForm />
+        <AddProductForm loggedInUserId={loggedInUserId} />
       {/* <Cart /> */}
     </div>
   );
