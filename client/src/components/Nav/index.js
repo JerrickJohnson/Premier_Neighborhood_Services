18eab34be225e61be212158513bc376d2be6b6e2
Daniel Covington <DanielmCovington@gmail.com>
2023-08-06 15:42:05 -0500

    updated home page and other styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,83 +1,70 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import './style.css';
 
 function Nav() {
 
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/message">
-              Messages
-            </Link>
+        <>
+          <li className="nav-item">
+            <Link className="nav-link" to="/message">Messages</Link>
           </li>
 
-          <li className="mx-1">
-            <Link to="/marketplace">
-              Marketplace
-            </Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/marketplace">Marketplace</Link>
           </li>
 
-          <li className="mx-1">
-            <Link to="/events">
-              Events
-            </Link>
-          </li>
-          
-        {/* added services button link */}
-          <li className="mx-1">
-            <Link to="/services">
-              Services
-            </Link>
-          </li>
-          {/* added payments button link */}
-          <li className="mx-1">
-            <Link to="/payments">
-              Payments
-            </Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/events">Events</Link>
           </li>
 
-          <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
+          <li className="nav-item">
+            <Link className="nav-link" to="/services">Services</Link>
           </li>
-        </ul>
+
+          <li className="nav-item">
+            <Link className="nav-link" to="/payments">Payments</Link>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="/" onClick={() => Auth.logout()}>Logout</a>
+          </li>
+        </>
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
+        <>
+          <li className="nav-item">
+            <Link className="nav-link" to="/signup">Signup</Link>
           </li>
-          <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
+
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">Login</Link>
           </li>
-        </ul>
+        </>
       );
     }
   }
 
   return (
-    <header className="flex-row px-1">
-      <h1>
-        <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          PNS
-        </Link>
-      </h1>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        <span role="img" aria-label="shopping bag"></span> Premier Neighborhood Services
+      </Link>
 
-      <nav>
-        {showNavigation()}
-      </nav>
-    </header>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          {showNavigation()}
+        </ul>
+      </div>
+    </nav>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
